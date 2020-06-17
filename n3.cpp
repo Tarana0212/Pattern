eafae7cdd88d42b20841a2e2da590fb441980fc5
Tarana0212 <61152129+Tarana0212@users.noreply.github.com>
2020-06-18 00:01:32 +0530

    Add files via upload

---
+++ n3.cpp
@@ -0,0 +1,31 @@
+/* 1111
+   0000
+   1111
+   0000
+   1111 */
+
+
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("enter range");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<n;j++)
+		{
+			if(i%2==1)
+			{
+			printf("1");
+		    }
+			else
+			{
+			
+			printf("0");
+		    }
+		}
+		printf("\n");
+	}
+	return 0;
+}
